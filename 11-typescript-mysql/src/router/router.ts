41344bb00ce2ed4fe58a7f7c5a67eacbcb7744cd
Esteban Tejeda <etejedaw@hotmail.com>
2021-01-16 03:24:44 -0300

    Finalizado ejercicio 11

--- 11-typescript-mysql/src/router/router.ts
+++ 11-typescript-mysql/src/router/router.ts
@@ -1,20 +1,40 @@
 import {Router, Request, Response} from 'express';
+import MySQL from '../mysql/mysql';
 
 const router = Router();
 
-router.get('/heroes', (req: Request, res: Response) => {
-    res.json({
-        ok: true,
-        message: "Fine"
+router.get('/customer', (req: Request, res: Response) => {
+    const query = `
+    SELECT *
+    FROM Customer`;
+    MySQL.execQuery(query, (err: any, customerDB: Object[]) => {
+        if(err) return res.status(400).json({
+            ok: false,
+            err
+        });
+        return res.json({
+            ok: true,
+            customerDB
+        });
     });
 });
 
-router.get('/heroes/:id', (req: Request, res: Response) => {
-    const id = req.params.id;
-    res.json({
-        ok: true,
-        message: "Fine",
-        id
+router.get('/customer/:id', (req: Request, res: Response) => {
+    let id = req.params.id;
+    id = MySQL.instance.cnn.escape(id);
+    const query = `
+    SELECT *
+    FROM Customer
+    WHERE idCustomer=${id}`;
+    MySQL.execQuery(query, (err: any, customerDB: Object[]) => {
+        if(err) return res.status(400).json({
+            ok: false,
+            err
+        });
+        return res.json({
+            ok: true,
+            customerDB
+        });
     });
 });
 
