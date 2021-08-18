972d7f9435446a1de28827f550917a36ebbb0a71
Esteban Tejeda <etejedaw@hotmail.com>
2021-08-18 14:22:50 -0400

    Añadido ejercicio 14-express

---
+++ 14-express/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRouters = require('./routes/admin');
+const shopRouters = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRouters);
+app.use(shopRouters);
+
+app.use((req, res, next) => {
+    res.status(404).send(`<h1>Page not found</h1>`)
+});
+
+app.listen(3000);
