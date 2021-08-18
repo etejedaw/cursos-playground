7bf98a7a39b57f9c2e309cb990c27b56dcccff96
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2021-08-18 14:22:50 -0400

    Añadido ejercicio 14-express

---
+++ 14-express/routes/admin.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/add-product', (req, res, next) => {
+    res.send(
+        `
+        <form action="/product" method="POST">
+        <input type="text" name="title">
+        <button type="submin">Add Product</button>
+        </form>
+        `
+    );
+});
+
+router.post('/add-product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports = router;
