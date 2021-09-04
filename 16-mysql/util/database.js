0ff614d462b0d370f52616e6f10d82f473e6c421
Esteban Tejeda <etejedaw@hotmail.com>
2021-09-03 21:50:23 -0400

    Cambios leves en ejercicio 16

--- 16-mysql/util/database.js
+++ 16-mysql/util/database.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'nodecomplete'
+    user: 'node_test',
+    database: 'node_mysql',
+    password: 'deno'
 });
 
 module.exports = pool.promise();
