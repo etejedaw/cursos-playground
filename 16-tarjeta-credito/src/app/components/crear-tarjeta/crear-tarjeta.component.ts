ba1b6437e225a72796148d163835daff0547a165
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2021-11-26 01:34:25 -0300

    Finalizado ejercicio 16

--- 16-tarjeta-credito/src/app/components/crear-tarjeta/crear-tarjeta.component.ts
+++ 16-tarjeta-credito/src/app/components/crear-tarjeta/crear-tarjeta.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import TarjetaCredito from '../../models/TarjetaCredito';
+import {TarjetaService} from "../../services/tarjeta.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-crear-tarjeta',
@@ -9,7 +10,12 @@ import TarjetaCredito from '../../models/TarjetaCredito';
 })
 export class CrearTarjetaComponent implements OnInit {
   form: FormGroup
-  constructor(private fb: FormBuilder) {
+  loading: boolean
+  titulo: string;
+  id: string | undefined;
+  constructor(private fb: FormBuilder, private _tarjetaService: TarjetaService, private toastr: ToastrService) {
+    this.titulo = "Agregar tarjeta";
+    this.loading = false;
     this.form = this.fb.group({
       titular: ['', Validators.required],
       numeroTarjeta: ['', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]],
@@ -19,16 +25,62 @@ export class CrearTarjetaComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this._tarjetaService.getTarjetaEdit().subscribe(data => {
+      this.id = data.id;
+      this.titulo = "Editar tarjeta";
+      this.form.patchValue({
+        titular: data.titular,
+        numeroTarjeta: data.numeroTarjeta,
+        fechaExpiracion: data.fechaExpiracion,
+        cvv: data.cvv
+      })
+    });
   }
 
-  crearTarjeta() {
-    const tarjetaCredito = new TarjetaCredito(
-      this.form.value.titular,
-      this.form.value.numeroTarjeta,
-      this.form.value.fechaExpiracion,
-      this.form.value.cvv
-    );
-    console.log(tarjetaCredito);
+  guardarTarjeta() {
+    if(this.id === undefined) this.agregarTarjeta();
+    else this.editarTarjeta(this.id);
+  }
+
+  agregarTarjeta(){
+    const tarjetaCredito = {
+      titular: this.form.value.titular,
+      numeroTarjeta: this.form.value.numeroTarjeta,
+      fechaExpiracion: this.form.value.fechaExpiracion,
+      cvv: this.form.value.cvv,
+      fechaCreacion: new Date(),
+      fechaActualizacion: new Date()
+    }
+    this.loading = true;
+    this._tarjetaService.guardarTarjeta(tarjetaCredito)
+      .then(() => {
+        this.loading = false;
+        this.toastr.success('La tarjeta fue registrada con éxito', 'Tarjeta registrada')
+        this.form.reset()
+      })
+      .catch(err => {
+        this.loading = false;
+        this.toastr.error('Ocurrió un error', 'Error');
+      });
+  }
+
+  editarTarjeta(id: string){
+    const tarjetaCredito = {
+      titular: this.form.value.titular,
+      numeroTarjeta: this.form.value.numeroTarjeta,
+      fechaExpiracion: this.form.value.fechaExpiracion,
+      cvv: this.form.value.cvv,
+      fechaActualizacion: new Date()
+    }
+    this.loading = true;
+    this._tarjetaService.editarTarjeta(id, tarjetaCredito)
+      .then(() => {
+        this.loading = false;
+        this.titulo = "Agregar Tarjeta";
+        this.form.reset();
+        this.id = undefined;
+      })
+      .catch(err => console.log(err));
   }
 
 }
--- 16-tarjeta-credito/src/app/components/listar-tarjeta/listar-tarjeta.component.ts
+++ 16-tarjeta-credito/src/app/components/listar-tarjeta/listar-tarjeta.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {TarjetaService} from "../../services/tarjeta.service";
+import TarjetaCredito from "../../models/TarjetaCredito";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-listar-tarjeta',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-tarjeta.component.css']
 })
 export class ListarTarjetaComponent implements OnInit {
+  listTarjetas: TarjetaCredito[];
 
-  constructor() { }
+  constructor(private _tarjetaService: TarjetaService, private toastr: ToastrService) {
+    this.listTarjetas = [];
+  }
 
   ngOnInit(): void {
+    this.obtenerTarjetas();
   }
+
+  obtenerTarjetas(){
+    this._tarjetaService.obtenerTarjetas().subscribe(doc => {
+      this.listTarjetas = [];
+      doc.forEach((element: any) => {
+        this.listTarjetas.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data()
+        });
+      });
+    });
+  }
+
+  eliminarTarjeta(id: any){
+    this._tarjetaService.eliminarTarjeta(id)
+      .then(() => {
+        this.toastr.error('La tarjeta fue eliminada con éxito', 'Registro eliminado')
+      })
+      .catch(err => console.log(err));
+  }
+
+  editarTarjeta(tarjeta: TarjetaCredito){
+    this._tarjetaService.addTarjetaEdit(tarjeta);
+  }
+
+
 
 }
