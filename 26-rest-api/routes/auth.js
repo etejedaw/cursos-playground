18f4cece99c98bf218b59ff7e20936a4c7a9216c
Esteban Tejeda <etejedaw@hotmail.com>
2022-01-13 13:16:46 -0300

    Finalizado ejercicio rest-api

---
+++ 26-rest-api/routes/auth.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const {body} = require("express-validator");
+
+const User = require("../models/user");
+const authController = require("../controllers/auth");
+
+const router = express.Router();
+
+router.put('/signup', [
+    body("email")
+        .isEmail()
+        .withMessage("Please enter a valid email")
+        .custom(value => User.findOne({email: value}).then(userDoc => {
+            if (userDoc) Promise.reject('Email Address already exists')
+        }))
+        .normalizeEmail(),
+    body("password").trim().isLength({min: 5}),
+    body("name").trim().not().isEmpty()
+], authController.signup);
+
+router.post('/login', authController.login);
+
+module.exports = router;
