e45a3ae5cf467d2109d626251850eb6eec9906b1
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2022-01-13 21:29:42 -0300

    Finalizado ejercicio base

--- 17-bases/src/app/dbz/main-page/main-page.component.ts
+++ 17-bases/src/app/dbz/main-page/main-page.component.ts
@@ -7,28 +7,12 @@ import { DbzService } from '../services/dbz.service';
   styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent{
-  personajes: Personaje[] = [
-    {
-      nombre: "Goku",
-      poder: 1500
-    },
-    {
-      nombre: "Vegeta",
-      poder: 7500
-    }
-  ];
 
   nuevo: Personaje = {
     nombre: "Maestro Roshi",
     poder: 1000
   }
 
-  agregarNuevoPersonaje(argumento: Personaje){
-    this.personajes.push(argumento);
-  }
-
-  constructor(private dbzService: DbzService){
-
-  }
+  constructor(){}
 
 }
