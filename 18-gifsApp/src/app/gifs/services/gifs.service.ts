ca6e944cb8abc33aa3ba446aa0d953515e84f0b0
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2022-01-17 21:46:13 -0300

    Avances en ejercicio gifsApp

--- 18-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 18-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,14 +7,23 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _historial: string[] = []
+  private apiKey = "3zMu33rK3ymkQgzbH5e5tpSh17pG7hPp";
+  public resultados: any[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get historial(){
     return [...this._historial];
   }
 
   buscarGifs(query: string){
-    this._historial.unshift(query);
+    query = query.trim().toLowerCase();
+    if(!this._historial.includes(query)){
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
+    }
+    this.http.get(`http://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10`)
+      .subscribe((resp: any) => this.resultados = resp.data);
   }
+  
 }
