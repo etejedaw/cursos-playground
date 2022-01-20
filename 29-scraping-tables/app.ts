a25f7b8c768b95732bc46f9e8dd8ac5a87d7d7f6
Esteban Tejeda <etejedaw@hotmail.com>
2022-01-20 16:09:00 -0300

    Finalizado ejercicio scraping tables

--- 29-scraping-tables/app.ts
+++ 29-scraping-tables/app.ts
@@ -23,4 +23,27 @@ const main = async () => {
     console.log(scrapedRows);
 }
 
-main();
+interface ScrapedRow2{
+    [index: string]: string;
+}
+
+const main2 = async () => {
+    const result = await request.get("https://www.codingwithstefan.com/table-example/");
+    const $ = cheerio.load(result);
+    const scrapedRows: ScrapedRow2[] = [];
+    const tableHeaders: String[] = [];
+    $("body > table > tbody > tr").each((index, element) => {
+        if (index === 0){
+            const ths = $(element).find("th");
+            ths.each((index, element) => tableHeaders.push($(element).text().toLowerCase()));
+            return;
+        }
+        const tds = $(element).find("td");
+        const tableRow: ScrapedRow2 = {};
+        tds.each((index, element) => tableRow[tableHeaders[index].toString()] = $(element).text())
+        scrapedRows.push(tableRow);
+    });
+    console.log(scrapedRows);
+}
+
+main2();
