07933b9f0d803034dea385aa3282ade08622908b
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2022-01-25 17:49:43 -0300

    Finalizado ejercicio paisesAPp

--- 19-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 19-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -12,10 +12,13 @@ export class PorPaisComponent {
   termino = "";
   hayError = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias = false;
 
   constructor(private paisService: PaisService) { }
 
   buscar(termino: string){
+    this.mostrarSugerencias = false;
     this.hayError = false;
     this.termino = termino;
     this.paisService.buscarPais(termino).subscribe({
@@ -29,7 +32,14 @@ export class PorPaisComponent {
 
   sugerencia(termino: string){
     this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+    this.paisService.buscarPais(termino)
+      .subscribe(paises => this.paisesSugeridos = paises.splice(0,3));
   }
 
+  buscarSugerido(termino: string){
+    this.buscar(termino);
+  }
 
 }
