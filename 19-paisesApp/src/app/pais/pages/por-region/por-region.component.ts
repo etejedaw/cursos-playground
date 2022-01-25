095ca90bf075c3e245e1f53e811f16a00831ee34
Esteban Tejeda <etejedaw@hotmail.com>
2022-01-25 17:49:43 -0300

    Finalizado ejercicio paisesAPp

--- 19-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
+++ 19-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styleUrls: ['./por-region.component.css']
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent{
 
-  constructor() { }
+  regiones: string[] = ["EU", "EFTA", "CARICOM", "PA", "AU", "USAN", "EEU", "AL", "ASEAN", "CAIS", "CEFTA", "NAFTA", "SAARC"];
+  regionActiva = "";
+  paises: Country[] = [];
+  
+  constructor(private paisServide: PaisService) { }
 
-  ngOnInit(): void {
+  activarRegion(region: string){
+    if(region === this.regionActiva) return;
+    this.regionActiva = region;
+    this.paises = [];
+    this.paisServide.buscarRegion(region).subscribe(paises => this.paises = paises);
+  }
+
+  getClaseCSS(region: string): string{
+    if(region === this.regionActiva) return 'btn btn-primary';
+    return 'btn btn-outline-primary';
   }
 
 }
