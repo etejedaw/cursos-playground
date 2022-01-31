17d3a48b04459f6eb193375f53ea614f4d3ef941
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2022-01-31 18:03:14 -0300

    Finalizado ejercicio de TDD scraping

---
+++ 31-tdd-scraper/parser.js
@@ -0,0 +1,13 @@
+const cheerio = require("cheerio");
+
+exports.listings = (html) => {
+    const $ = cheerio.load(html);
+    return $(".result-info").map((index, element) => {
+        const titleElement = $(element).find(".result-title.hdrlnk");
+        const title = titleElement.text().trim();
+        const url = titleElement.attr("href");
+        const hood = $(element).find(".result-hood").text().trim();
+        const datePosted = new Date($(element).find(".result-date").attr("datetime"));
+        return {title, url, hood, datePosted};
+    }).get();
+}
