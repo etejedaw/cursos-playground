a1e6f121947fe6ce4619ff50d2bb99e4e2b6b3fc
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2022-02-01 16:31:52 -0300

    Finalizado ejercicios de pipes personalizados

--- 20-pipesApp/src/app/shared/menu/menu.component.ts
+++ 20-pipesApp/src/app/shared/menu/menu.component.ts
@@ -18,12 +18,13 @@ export class MenuComponent implements OnInit {
         items: [
           {label: "Textos y fechas", icon: "pi pi-align-left", routerLink: "/"},
           {label: "Números", icon: "pi pi-dollar", routerLink: "numeros"},
-          {label: "No comunes", icon: "pi pi-globe", routerLink: "no-comunes"}
+          {label: "No comunes", icon: "pi pi-globe", routerLink: "no-comunes"},
         ]
       },
       {
         label: "Pipes personalizados",
-        icon: "pi pi-cog"
+        icon: "pi pi-cog",
+        routerLink: "ordenar"
       }
     ];
   }
