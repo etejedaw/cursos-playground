cff7ed407cfdf8cf22d6a60b5d0ec9e60bf57743
Esteban Tejeda <etejedaw@hotmail.com>
2022-02-01 16:31:52 -0300

    Finalizado ejercicios de pipes personalizados

--- 20-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
+++ 20-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroes } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html',
   styleUrls: ['./ordenar.component.css']
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
+  enMayusculas = true;
+  ordenarPor: string = "sin valor";
+  heroes: Heroes[] = [
+    {
+      nombre: "Superman",
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: "Batman",
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: "Robin",
+      vuela: false,
+      color: Color.verde
+    },
+    {
+      nombre: "Daredevil",
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: "Linterna Verde",
+      vuela: true,
+      color: Color.verde
+    }
+  ];
 
-  constructor() { }
+  cambiarMayuscula(){
+    this.enMayusculas = !this.enMayusculas;
+  }
 
-  ngOnInit(): void {
+  cambiarOrden(valor: string){
+    this.ordenarPor = valor;
   }
 
 }
