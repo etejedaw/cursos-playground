35543783b2136df484ac3b010103d843b2c9f2b8
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2022-03-12 03:06:17 -0300

    Cambios leves en llamado a enum dentro de 01-Factory-Method

--- 01-SPD/01-Factory-Method/app.ts
+++ 01-SPD/01-Factory-Method/app.ts
@@ -1,11 +1,13 @@
 import RoadLogistics from './models/Factory/RoadLogistics';
 import SeaLogistics from './models/Factory/SeaLogistics';
+import RoadType from "./enums/road-type"
+import SeaType from "./enums/sea-type"
 
 const roadLogistics = new RoadLogistics();
 const seaLogistics = new SeaLogistics();
 
-let c = roadLogistics.createTransport(1, "patente-01");
-let b = seaLogistics.createTransport(0, "patente-02");
+let c = roadLogistics.createTransport(RoadType.Truck, "patente-01");
+let b = seaLogistics.createTransport(SeaType.Ship, "patente-02");
 
 console.log(c.deliver());
 console.log(b.getModel());
