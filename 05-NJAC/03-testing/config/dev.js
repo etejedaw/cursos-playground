02622dab82b630df0977ead64a6637f4b6bc0fed
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2022-08-12 17:39:02 -0400

    05-NJAC: Finalizado ejercicio de testing

---
+++ 05-NJAC/03-testing/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  mongoURI: 'mongodb://test_user:test_password@ac-doziowx-shard-00-00.jbywrae.mongodb.net:27017,ac-doziowx-shard-00-01.jbywrae.mongodb.net:27017,ac-doziowx-shard-00-02.jbywrae.mongodb.net:27017/blog-dev?ssl=true&replicaSet=atlas-mhukyb-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+};
