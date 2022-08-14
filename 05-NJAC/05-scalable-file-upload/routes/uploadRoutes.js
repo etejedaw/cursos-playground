d55917cc590e704736f5324367d39ca8497c2aa7
Esteban Tejeda <etejedaw@hotmail.com>
2022-08-14 08:45:08 -0400

    05-NJAC: Curso Finalizado

---
+++ 05-NJAC/05-scalable-file-upload/routes/uploadRoutes.js
@@ -0,0 +1,23 @@
+const AWS = require("aws-sdk");
+const uuid = require("uuid/v1");
+const requireLogin = require("../middlewares/requireLogin");
+const keys = require("../config/keys");
+
+const s3 = new AWS.S3({
+    credentials: {
+        accessKeyId: keys.accessKeyId,
+        secretAccessKey: keys.secretAccessKey
+    },
+    region: "sa-east-1"
+})
+
+module.exports = app => {
+    app.get("/api/upload", requireLogin, (req, res) => {
+        const key = `${req.user.id}/${uuid()}.jpeg`;
+        s3.getSignedUrl("putObject", {
+            Bucket: "my-blog-bucket-95",
+            ContentType: "image/jpeg",
+            Key: key
+        }, (err, url) => res.send({key, url}));
+    });
+}
