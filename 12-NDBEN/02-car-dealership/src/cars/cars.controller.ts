e55e12a3099d60288d78cf929f98958484464d96
Esteban Tejeda <esteban.tejeda@alumnos.uach.cl>
2022-08-16 11:49:40 -0400

    12-NDBEN: Finalizado Ejercicio 2

---
+++ 12-NDBEN/02-car-dealership/src/cars/cars.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CarsService } from './cars.service';
+
+@Controller('cars')
+export class CarsController {
+  constructor(private readonly carsService: CarsService) {}
+
+  @Get()
+  getAllCars() {
+    return this.carsService.findAll();
+  }
+
+  @Get(':id')
+  getCarById(@Param('id', ParseIntPipe) id: number) {
+    return this.carsService.findOnById(id);
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return body;
+  }
+
+  @Patch(':id')
+  updateCar(
+    @Param('id', ParseIntPipe)
+    @Body()
+    body: any,
+  ) {
+    return body;
+  }
+
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return {
+      method: 'DELETE',
+      id,
+    };
+  }
+}
