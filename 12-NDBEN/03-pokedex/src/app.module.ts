b35eae17a1aefdb3972a8ebb8e001dc44c475066
Esteban Tejeda <etejedaw@hotmail.com>
2022-08-22 09:43:21 -0400

    12-NDBEN: Avancede en ejercicio 6

--- 12-NDBEN/03-pokedex/src/app.module.ts
+++ 12-NDBEN/03-pokedex/src/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ validationSchema: JoiValidationSchema }),
     ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
-    MongooseModule.forRoot('mongodb://localhost:27017'),
+    MongooseModule.forRoot(process.env.MONGODB),
     PokemonModule,
     CommonModule,
     SeedModule,
