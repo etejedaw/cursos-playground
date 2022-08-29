d3845d11ba0e82c92b52481e13fc4b86e4325b2c
Esteban Tejeda <etejedaw@hotmail.com>
2022-08-29 12:25:41 -0400

    10-ADCAE: Finalizado ejercicio 6
    Relaciones en TypeORM

--- 12-NDBEN/04-teslo-shop/src/products/products.controller.ts
+++ 12-NDBEN/04-teslo-shop/src/products/products.controller.ts
@@ -29,8 +29,8 @@ export class ProductsController {
   }
 
   @Get(':id')
-  findOne(@Param('id', ParseUUIDPipe) id: string) {
-    return this.productsService.findOne(id);
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOnePlain(id);
   }
 
   @Patch(':id')
