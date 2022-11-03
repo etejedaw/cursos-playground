b6eaceb8a08bb60102067412515c2baf32a85fcf
Esteban Tejeda <etejedaw@hotmail.com>
2022-11-02 21:20:03 -0300

    10-ADCAE: Avances en ejeren ejercicio 13

--- 10-ADCAE/05-heroesApp/src/app/heroes/heroes.module.ts
+++ 10-ADCAE/05-heroesApp/src/app/heroes/heroes.module.ts
@@ -6,6 +6,7 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,6 @@ import { HeroesRoutingModule } from './heroes-routing.module';
     HomeComponent,
     ListadoComponent,
   ],
-  imports: [CommonModule, HeroesRoutingModule],
+  imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
 export class HeroesModule {}
