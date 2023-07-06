9bfc55ef071452d20f685cfd9cbe692896a0f634
Esteban Tejeda <etejedaw@hotmail.com>
2023-07-06 02:07:01 -0400

    11-TCANC: Actualización de carpetas

---
+++ 11-TCANC/02-nestjs_admin/backend/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'db',
+      port: 3306,
+      username: 'root',
+      password: 'toor',
+      database: 'admin',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
