36be62a8e8584e4cf06141af96e70a0c26567989
Esteban Tejeda <etejedaw@hotmail.com>
2024-04-25 03:22:06 -0400

    08-NTCDG: Finalizada Sección 13

---
+++ 08-NTCDG/05-my-car-value/test/auth.e2e-spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication System (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/auth/signup (POST): handles a signup request', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: 'test-e2e@test.com', password: '1234' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toEqual('test-e2e@test.com');
+      });
+  });
+});
