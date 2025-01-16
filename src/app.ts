c784bdf94f8b151c1bb6eaf3bc72b33d3cf96962
Esteban Tejeda <etejedaw@hotmail.com>
2025-01-15 22:10:04 -0300

    add env variable

--- src/app.ts
+++ src/app.ts
@@ -5,3 +5,8 @@ export function sum(a: number, b: number) {
 export function multiply(a: number, b: number) {
 	return a * b;
 }
+
+export function addTax(amount: number) {
+	const tax = Number(process.env.TAX);
+	return amount * tax;
+}
