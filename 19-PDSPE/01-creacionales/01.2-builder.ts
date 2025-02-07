b1f74b1d568b1f7c808ba84ab2ebf31cbc00ff58
Esteban Tejeda <etejedaw@hotmail.com>
2025-02-06 22:12:07 -0300

    19-PDSPE: Finalizada sección 1

--- 19-PDSPE/01-creacionales/01.2-builder.ts
+++ 19-PDSPE/01-creacionales/01.2-builder.ts
@@ -50,24 +50,36 @@ class QueryBuilder {
 	}
 
 	select(...fields: string[]): QueryBuilder {
-		throw new Error("Method not implemented.");
+		this.fields = fields;
+		return this;
 	}
 
 	where(condition: string): QueryBuilder {
-		throw new Error("Method not implemented.");
+		this.conditions.push(condition);
+		return this;
 	}
 
 	orderBy(field: string, direction: "ASC" | "DESC" = "ASC"): QueryBuilder {
-		throw new Error("Method not implemented.");
+		this.orderFields.push(`${field} ${direction}`);
+		return this;
 	}
 
 	limit(count: number): QueryBuilder {
-		throw new Error("Method not implemented.");
+		this.limitCount = count;
+		return this;
 	}
 
 	execute(): string {
-		// Select id, name, email from users where age > 18 and country = 'Cri' order by name ASC limit 10;
-		throw new Error("Method not implemented.");
+		const select = `SELECT ${this.fields.join(", ") || "*"}`;
+		const from = `FROM ${this.table}`;
+		const where = this.conditions.length
+			? `WHERE ${this.conditions.join(" AND ")}`
+			: "";
+		const orderBy = this.orderFields.length
+			? `ORDER BY ${this.orderFields.join(" AND ")}`
+			: "";
+		const limit = this.limitCount ? `LIMIT ${this.limitCount}` : "";
+		return `${select} ${from} ${where} ${orderBy} ${limit}`.trim();
 	}
 }
 
