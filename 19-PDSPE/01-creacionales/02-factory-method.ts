b2464a37014e6c094dfa5912f164fc0231df962b
Esteban Tejeda <etejedaw@hotmail.com>
2025-02-06 22:58:51 -0300

    19-PDSPE: Finalizada sección 2

--- 19-PDSPE/01-creacionales/02-factory-method.ts
+++ 19-PDSPE/01-creacionales/02-factory-method.ts
@@ -12,3 +12,79 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Hamburguer {
+	prepare(): void;
+}
+
+class ChickenHamburguer implements Hamburguer {
+	prepare() {
+		console.log("Preparando hamburguesa de %cpollo", COLORS.yellow);
+	}
+}
+
+class BeefHamburguer implements Hamburguer {
+	prepare() {
+		console.log("Preparando hamburguesa de %cvacuno", COLORS.brown);
+	}
+}
+class BeanHamburguer implements Hamburguer {
+	prepare() {
+		console.log("Preparando hamburguesa de %cfrijoles", COLORS.black);
+	}
+}
+
+abstract class Restaurant {
+	protected abstract createHamburguer(): Hamburguer;
+
+	orderHamburguer() {
+		const hamburguer = this.createHamburguer();
+		hamburguer.prepare();
+	}
+}
+
+class ChickenRestaurant extends Restaurant {
+	createHamburguer(): Hamburguer {
+		return new ChickenHamburguer();
+	}
+}
+
+class BeefRestaurant extends Restaurant {
+	createHamburguer(): Hamburguer {
+		return new BeefHamburguer();
+	}
+}
+
+class BeanRestaurant extends Restaurant {
+	createHamburguer(): Hamburguer {
+		return new BeanHamburguer();
+	}
+}
+
+function main() {
+	let restaurant: Restaurant;
+
+	const burguerType = prompt(
+		"Qué tipo de hamburguesa quieres? (chicken/beef/bean)"
+	);
+
+	switch (burguerType) {
+		case "chicken":
+			restaurant = new ChickenRestaurant();
+			break;
+		case "beef":
+			restaurant = new BeefRestaurant();
+			break;
+		case "bean":
+			restaurant = new BeanRestaurant();
+			break;
+		default:
+			throw new Error("Opción no valida");
+	}
+
+	restaurant.orderHamburguer();
+}
+
+main();
