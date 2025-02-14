052c2be346f38a2e79e00a7531159403be07b8ed
Esteban Tejeda <etejedaw@hotmail.com>
2025-02-14 00:27:19 -0300

    finalizado ejercicio patrón bridge

--- 19-PDSPE/02-estructurales/02-bridge.ts
+++ 19-PDSPE/02-estructurales/02-bridge.ts
@@ -9,3 +9,71 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+
+interface Ability {
+	use(): void;
+}
+
+class SwordAttack implements Ability {
+	use(): void {
+		console.log("*Ataca con una espada*");
+	}
+}
+
+class MagicSpell implements Ability {
+	use(): void {
+		console.log("*Lanza un hechizo mágico*");
+	}
+}
+
+class AxeAttack implements Ability {
+	use(): void {
+		console.log("*Ataca con un hacha*");
+	}
+}
+class FireballSpell implements Ability {
+	use(): void {
+		console.log("*Lanza una bola de fuego*");
+	}
+}
+
+abstract class Character {
+	protected ability: Ability;
+
+	constructor(ability: Ability) {
+		this.ability = ability;
+	}
+
+	setAbility(ability: Ability) {
+		this.ability = ability;
+	}
+
+	abstract performHability(): void;
+}
+
+class Warrior extends Character {
+	override performHability(): void {
+		console.log("El guerrero está listo para luchar");
+		this.ability.use();
+	}
+}
+
+class Mage extends Character {
+	override performHability(): void {
+		console.log("El mago prepara su magia");
+		this.ability.use();
+	}
+}
+
+function main() {
+	const warrior = new Warrior(new SwordAttack());
+	warrior.performHability();
+
+	warrior.setAbility(new AxeAttack());
+	warrior.performHability();
+
+	const mage = new Mage(new FireballSpell());
+	mage.performHability();
+}
+
+main();
