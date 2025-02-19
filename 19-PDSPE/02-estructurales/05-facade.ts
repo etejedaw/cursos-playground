c7e4bc6856812dee62880d89bf7cdb973e1040d6
Esteban Tejeda <etejedaw@hotmail.com>
2025-02-18 23:46:59 -0300

    19-PDSPE: finalizado patrón facade

--- 19-PDSPE/02-estructurales/05-facade.ts
+++ 19-PDSPE/02-estructurales/05-facade.ts
@@ -11,3 +11,93 @@
  *
  * https://refactoring.guru/es/design-patterns/facade
  */
+
+class Projector {
+	turnOn() {
+		console.log("Proyector encendido");
+	}
+
+	turnOff() {
+		console.log("Proyector Apagado");
+	}
+}
+
+class SoundSystem {
+	on() {
+		console.log("Sistema de sonido encendido");
+	}
+
+	off() {
+		console.log("Sistema de sonido apagado");
+	}
+}
+
+class VideoPlayer {
+	on() {
+		console.log("Reproductor de video encendido");
+	}
+
+	play(movie: string) {
+		console.log(`Reproduciendo ${movie}`);
+	}
+
+	stop() {
+		console.log("Película detenida");
+	}
+
+	off() {
+		console.log("Reproductor de video apagado");
+	}
+}
+
+class PopcornMaker {
+	poppingPopCorn() {
+		console.log("Haciendo palomitas");
+	}
+
+	turnOffPoppingPopCorn() {
+		console.log("Deteniendo las palomitas");
+	}
+}
+
+class HomeTheaterFacade {
+	constructor(
+		private projector: Projector,
+		private soundSystem: SoundSystem,
+		private videoPlayer: VideoPlayer,
+		private popcornMaker: PopcornMaker
+	) {}
+
+	watchMovie(movie: string) {
+		console.log("Preparando para ver la película\n");
+		this.projector.turnOn();
+		this.soundSystem.on();
+		this.popcornMaker.poppingPopCorn();
+		this.videoPlayer.on();
+		this.videoPlayer.play(movie);
+		console.log("> Disfrute la película\n");
+	}
+
+	endWatchingMovie() {
+		console.log("Preparando para detener la película\n");
+		this.projector.turnOff();
+		this.soundSystem.off();
+		this.popcornMaker.turnOffPoppingPopCorn();
+		this.videoPlayer.stop();
+		this.videoPlayer.off();
+		console.log("> Sistema Apagado\n");
+	}
+}
+
+function main() {
+	const homeTheaterFacade = new HomeTheaterFacade(
+		new Projector(),
+		new SoundSystem(),
+		new VideoPlayer(),
+		new PopcornMaker()
+	);
+	homeTheaterFacade.watchMovie("The Avengers");
+	homeTheaterFacade.endWatchingMovie();
+}
+
+main();
