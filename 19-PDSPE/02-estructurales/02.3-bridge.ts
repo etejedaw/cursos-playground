1e278c20c2a39162237dd6d0aa8fe47b2bdaa9ef
Esteban Tejeda <etejedaw@hotmail.com>
2025-02-18 22:48:57 -0300

    19-PDSPE: finalizado patrón composite

--- 19-PDSPE/02-estructurales/02.3-bridge.ts
+++ 19-PDSPE/02-estructurales/02.3-bridge.ts
@@ -9,3 +9,61 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+import { COLORS } from "../helpers/colors.ts";
+
+// 1. Interfaz NotificationChannel
+// Define el método `send`, que cada canal de comunicación implementará.
+interface NotificationChannel {
+	send(message: string): void;
+}
+
+// 2. Implementaciones de Canales de Comunicación
+
+class EmailChannel implements NotificationChannel {
+	send(message: string): void {
+		console.log(`Enviando correo electrónico: ${message}`);
+	}
+}
+
+class SMSChannel implements NotificationChannel {
+	send(message: string): void {
+		console.log(`Enviando SMS: ${message}`);
+	}
+}
+
+class PushNotificationChannel implements NotificationChannel {
+	send(message: string): void {
+		console.log(`Enviando Push: ${message}`);
+	}
+}
+
+// 3. Clase Abstracta Notification
+// Define la propiedad `channel` y el método `notify`
+
+abstract class Notification {
+	constructor(protected channels: NotificationChannel[]) {}
+
+	abstract notify(message: string): void;
+	abstract addChannel(channel: NotificationChannel): void;
+}
+
+class AlertNotification extends Notification {
+	notify(message: string): void {
+		console.log("\n%c Notificación de alerta", COLORS.red);
+		this.channels.forEach(channel => channel.send(message));
+	}
+
+	addChannel(channel: NotificationChannel): void {
+		this.channels.push(channel);
+	}
+}
+
+function main() {
+	const channels = [new EmailChannel(), new SMSChannel()];
+
+	const alert = new AlertNotification(channels);
+
+	alert.notify("Alerta!");
+}
+
+main();
--- 19-PDSPE/02-estructurales/03-composite.ts
+++ 19-PDSPE/02-estructurales/03-composite.ts
@@ -13,3 +13,55 @@
  * https://refactoring.guru/es/design-patterns/composite
  *
  */
+
+interface FileSystemComponent {
+	showDetails(ident?: string): void;
+}
+
+class Fichero implements FileSystemComponent {
+	constructor(private name: string) {}
+
+	showDetails(ident?: string): void {
+		console.log(`${ident}-Archivo: ${this.name}`);
+	}
+}
+
+class Folder implements FileSystemComponent {
+	private contents: FileSystemComponent[] = [];
+	constructor(private name: string) {}
+
+	showDetails(ident: string = ""): void {
+		console.log(`${ident}+Carpeta: ${this.name}`);
+		this.contents.forEach(component => component.showDetails(ident + "\t"));
+	}
+
+	add(component: FileSystemComponent) {
+		this.contents.push(component);
+	}
+}
+
+function main() {
+	const file1 = new Fichero("archivo1.txt");
+	const file2 = new Fichero("archivo2.txt");
+	const file3 = new Fichero("archivo3.txt");
+	const file4 = new Fichero("archivo4.txt");
+
+	const folder1 = new Folder("Carpeta 1");
+	folder1.add(file1);
+	folder1.add(file2);
+
+	const folder2 = new Folder("Carpeta 2");
+	folder2.add(file3);
+
+	const folder3 = new Folder("Carpeta 3");
+	folder3.add(file4);
+	folder2.add(folder3);
+
+	const rootFolder = new Folder("/root");
+	rootFolder.add(folder1);
+	rootFolder.add(folder2);
+
+	rootFolder.showDetails();
+}
+
+main();
