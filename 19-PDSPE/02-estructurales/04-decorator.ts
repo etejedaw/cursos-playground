01e5734951c672387bf90d897e26cfca78d96d97
Esteban Tejeda <etejedaw@hotmail.com>
2025-02-18 23:26:20 -0300

    19-PDSPE: finalizado patrón decorator

--- 19-PDSPE/02-estructurales/04-decorator.ts
+++ 19-PDSPE/02-estructurales/04-decorator.ts
@@ -11,3 +11,62 @@
  *
  * https://refactoring.guru/es/design-patterns/decorator
  */
+
+import { COLORS } from "../helpers/colors.ts";
+
+interface Notification {
+	send(message: string): void;
+}
+
+class BasicNotification implements Notification {
+	send(message: string): void {
+		console.log(`Enviando notificación básica: %c${message}`, COLORS.blue);
+	}
+}
+
+abstract class NotificationDecorator implements Notification {
+	constructor(protected notificacion: Notification) {}
+
+	send(message: string): void {
+		this.notificacion.send(message);
+	}
+}
+
+class EmailDecorator extends NotificationDecorator {
+	private sendEmail(message: string) {
+		console.log(
+			`Enviando notificación por email: %c${message}`,
+			COLORS.brown
+		);
+	}
+
+	override send(message: string): void {
+		super.send(message);
+		this.sendEmail(message);
+	}
+}
+
+class SmsDecorator extends NotificationDecorator {
+	private sendSms(message: string) {
+		console.log(
+			`Enviando notificación por sms: %c${message}`,
+			COLORS.yellow
+		);
+	}
+
+	override send(message: string): void {
+		super.send(message);
+		this.sendSms(message);
+	}
+}
+
+function main() {
+	let notificacion: Notification = new BasicNotification();
+
+	notificacion = new EmailDecorator(notificacion);
+	notificacion = new SmsDecorator(notificacion);
+
+	notificacion.send("Hello World!");
+}
+
+main();
