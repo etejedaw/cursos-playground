bc466de4b9317b54324df30ad16b8087a264f10a
Esteban Tejeda <etejedaw@hotmail.com>
2025-04-13 23:04:16 -0400

    19-PDSPE: finalizado ejercicio patrón state

--- 19-PDSPE/03-comportamiento/07-state.ts
+++ 19-PDSPE/03-comportamiento/07-state.ts
@@ -1,3 +1,5 @@
+import { sleep } from "../helpers/sleep.ts";
+
 /**
  * ! Patrón State
  * Este patrón permite a un objeto cambiar su comportamiento
@@ -18,3 +20,136 @@
  *  * Entregando Producto,
  * * y su comportamiento varía dependiendo del estado actual.
  */
+
+interface State {
+	name: string;
+	insertMoney(): void;
+	selectProduct(): void;
+	dispenseProduct(): void;
+}
+
+class VendingMachine {
+	private state: State;
+
+	constructor() {
+		this.state = new WaitingForMoney(this);
+	}
+
+	insertMoney() {
+		this.state.insertMoney();
+	}
+
+	selectProduct() {
+		this.state.selectProduct();
+	}
+
+	dispenseProduct() {
+		this.state.dispenseProduct();
+	}
+
+	setState(state: State) {
+		this.state = state;
+		console.log(`El estado cambió a: ${state.name}`);
+	}
+
+	getStateName() {
+		return this.state.name;
+	}
+}
+
+class WaitingForMoney implements State {
+	name = "Esperando Dinero";
+
+	constructor(private vendingMachine: VendingMachine) {}
+
+	insertMoney(): void {
+		console.log("Dinero Insertado: Ahora puedes seleccionar un producto");
+		this.vendingMachine.setState(new ProductSelected(this.vendingMachine));
+	}
+
+	selectProduct(): void {
+		console.log("Primero debes insertar dinero");
+	}
+
+	dispenseProduct(): void {
+		console.log("Primero debes insertar dinero");
+	}
+}
+
+class ProductSelected implements State {
+	name = "Seleccionando Producto";
+
+	constructor(private vendingMachine: VendingMachine) {}
+
+	insertMoney(): void {
+		console.log("Selecciona un producto [Dinero ya procesado]");
+	}
+
+	selectProduct(): void {
+		console.log("Dinero Insertado: Ahora puedes seleccionar un producto");
+		this.vendingMachine.setState(
+			new DispensingProduct(this.vendingMachine)
+		);
+	}
+
+	dispenseProduct(): void {
+		console.log("Selecciona un producto [Dinero ya procesado]");
+	}
+}
+
+class DispensingProduct implements State {
+	name = "Despachando producto";
+
+	constructor(private vendingMachine: VendingMachine) {}
+
+	insertMoney(): void {
+		console.log("Espera a que se entregue el producto");
+	}
+
+	selectProduct(): void {
+		console.log("Espera a que se entregue el producto");
+	}
+
+	dispenseProduct(): void {
+		console.log("Producto despachado. Cambiando a estado EsperandoDinero");
+		this.vendingMachine.setState(new WaitingForMoney(this.vendingMachine));
+	}
+}
+
+async function main() {
+	const vendingMachine = new VendingMachine();
+	let selectedOption: string | null = "4";
+
+	do {
+		console.clear();
+
+		selectedOption = prompt(
+			`
+            1. Insertar dinero
+            2. Seleccionar producto
+            3. Dispensar producto
+            4. Salir
+            `
+		);
+
+		switch (selectedOption) {
+			case "1":
+				vendingMachine.insertMoney();
+				break;
+			case "2":
+				vendingMachine.selectProduct();
+				break;
+			case "3":
+				vendingMachine.dispenseProduct();
+				break;
+			case "4":
+				console.log("Saliendo de sistema");
+				break;
+			default:
+				console.log("Opción no válida");
+		}
+		await sleep(2000);
+	} while (selectedOption !== "4");
+}
+
+void main();
--- 19-PDSPE/03-comportamiento/07.2-state.ts
+++ 19-PDSPE/03-comportamiento/07.2-state.ts
@@ -7,7 +7,8 @@
  * * y debe cambiar su comportamiento en tiempo de ejecución dependiendo de ese estado.
  */
 
-import { COLORS, sleep } from '../helpers/index.ts';
+import { COLORS } from "../helpers/colors.ts";
+import { sleep } from "../helpers/sleep.ts";
 
 /**
  * !Objetivo:
@@ -24,128 +25,121 @@ import { COLORS, sleep } from '../helpers/index.ts';
 
 // Interfaz State
 interface State {
-  name: string;
+	name: string;
 
-  open(): void;
-  close(): void;
+	open(): void;
+	close(): void;
 }
 
 // Clase Context - AutomaticDoor
 class AutomaticDoor {
-  private state: State;
+	private state: State;
 
-  constructor() {
-    this.state = new Closed(this);
-  }
+	constructor() {
+		this.state = new Closed(this);
+	}
 
-  setState(state: State): void {
-    this.state = state;
-    console.log(`%cEstado cambiado a: ${state.name}`, COLORS.green);
-  }
+	setState(state: State): void {
+		this.state = state;
+		console.log(`%cEstado cambiado a: ${state.name}`, COLORS.green);
+	}
 
-  open(): void {
-    this.state.open();
-  }
+	open(): void {
+		this.state.open();
+	}
 
-  close(): void {
-    this.state.close();
-  }
+	close(): void {
+		this.state.close();
+	}
 
-  getStateName(): string {
-    return this.state.name;
-  }
+	getStateName(): string {
+		return this.state.name;
+	}
 }
 
 // Estado 1 - Cerrada
 class Closed implements State {
-  private door: AutomaticDoor;
-  public name: string;
+	public name = "Estado 1: Puerta Cerrada";
 
-  open(): void {
-    console.log('Abriendo la puerta...');
-    // TODO: Implementar lógica para colocar el estado en abriendo la puerta (Opening)
-  }
+	constructor(private door: AutomaticDoor) {}
 
-  close(): void {
-    console.log('La puerta ya está cerrada.');
-  }
+	open(): void {
+		console.log("Abriendo la puerta...");
+		this.door.setState(new Opening(this.door));
+	}
+
+	close(): void {
+		console.log("La puerta ya está cerrada.");
+	}
 }
 
 // Estado 2 - Abriéndose
 class Opening implements State {
-  public name: string;
-  private door: AutomaticDoor;
+	public name = "Estado 2: Puerta Abriéndose";
 
-  constructor(door: AutomaticDoor) {
-    //TODO: asignar door y name = Abriendo
-    this.afterOpen();
-  }
+	constructor(private door: AutomaticDoor) {
+		this.afterOpen();
+	}
 
-  private async afterOpen() {
-    await sleep(3000);
+	private async afterOpen() {
+		await sleep(3000);
 
-    console.log('La puerta se ha abierto.');
-    // TODO: Implementar lógica para abrir la puerta (Open)
-  }
+		console.log("La puerta se ha abierto.");
+		this.door.setState(new Open(this.door));
+	}
 
-  open(): void {
-    console.log('La puerta ya se está abriendo.');
-  }
+	open(): void {
+		console.log("La puerta ya se está abriendo.");
+	}
 
-  close(): void {
-    console.log('La puerta no puede cerrarse mientras se abre.');
-  }
+	close(): void {
+		console.log("La puerta no puede cerrarse mientras se abre.");
+	}
 }
 
 // Estado 3 - Abierta
 class Open implements State {
-  private door: AutomaticDoor;
-  public name: string;
+	public name = "Abierta";
 
-  constructor(door: AutomaticDoor) {
-    this.name = 'Abierta';
-  }
+	constructor(private door: AutomaticDoor) {}
 
-  open(): void {
-    console.log('La puerta ya está abierta.');
-  }
+	open(): void {
+		console.log("La puerta ya está abierta.");
+	}
 
-  close(): void {
-    console.log('Cerrando la puerta...');
-    // TODO: Implementar lógica para cerrar la puerta (Closing)
-  }
+	close(): void {
+		console.log("Cerrando la puerta...");
+		this.door.setState(new Closing(this.door));
+	}
 }
 
 // Estado 4 - Cerrándose
 class Closing implements State {
-  public name: string;
+	public name = "Cerrándose";
 
-  constructor(door: AutomaticDoor) {
-    this.door = door;
-    this.name = 'Cerrándose';
-  }
+	constructor(private door: AutomaticDoor) {}
 
-  open(): void {
-    console.log('Detectando movimiento. Abriendo la puerta nuevamente...');
-    //TODO: Implementar lógica para abrir la puerta (Opening)
-  }
+	open(): void {
+		console.log("Detectando movimiento. Abriendo la puerta nuevamente...");
+		this.door.setState(new Opening(this.door));
+	}
 
-  close(): void {
-    console.log('La puerta se ha cerrado.');
-    // TODO: Implementar lógica para cerrar la puerta (Closed)
-  }
+	close(): void {
+		console.log("La puerta se ha cerrado.");
+		this.door.setState(new Closed(this.door));
+	}
 }
 
 // Código Cliente para probar el patrón State
 async function main() {
-  const door = new AutomaticDoor();
+	const door = new AutomaticDoor();
 
-  let selectedOption: string | null = '3';
+	let selectedOption: string | null = "3";
 
-  do {
-    console.clear();
-    console.log(`Estado actual: ${door.getStateName()}`);
-    selectedOption = prompt(`
+	do {
+		console.clear();
+		console.log(`Estado actual: ${door.getStateName()}`);
+		selectedOption = prompt(`
       1. Abrir puerta
       2. Cerrar puerta
       3. Salir
@@ -153,23 +147,23 @@ async function main() {
       Selecciona una opción: 
     `);
 
-    switch (selectedOption) {
-      case '1':
-        door.open();
-        break;
-      case '2':
-        door.close();
-        break;
-      case '3':
-        console.log('Saliendo del simulador...');
-        break;
-      default:
-        console.log('Opción no válida.');
-        break;
-    }
+		switch (selectedOption) {
+			case "1":
+				door.open();
+				break;
+			case "2":
+				door.close();
+				break;
+			case "3":
+				console.log("Saliendo del simulador...");
+				break;
+			default:
+				console.log("Opción no válida.");
+				break;
+		}
 
-    await sleep(2000);
-  } while (selectedOption !== '3');
+		await sleep(2000);
+	} while (selectedOption !== "3");
 }
 
 main();
