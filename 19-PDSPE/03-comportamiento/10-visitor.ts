b56e0924ccf193784ca9dee1084a8408adf7118b
Esteban Tejeda <etejedaw@hotmail.com>
2025-04-14 21:51:03 -0400

    19-PDSPE: Curso Finalizado

--- 19-PDSPE/03-comportamiento/10-visitor.ts
+++ 19-PDSPE/03-comportamiento/10-visitor.ts
@@ -24,3 +24,135 @@
  * de la atracción como del tipo de visitante,
  * sin modificar las clases originales.
  */
+
+interface Visitor {
+	visitRollerCoaster(rollerCoaster: RollerCoaster): void;
+	visitHauntedHouse(hauntedHouse: HauntedHouse): void;
+	visitFerrisWheel(ferrisWheel: FerrisWheel): void;
+}
+
+interface Attraction {
+	accept(visitor: Visitor): void;
+}
+
+class RollerCoaster implements Attraction {
+	private price = 50;
+
+	getPrice() {
+		return this.price;
+	}
+
+	accept(visitor: Visitor): void {
+		visitor.visitRollerCoaster(this);
+	}
+}
+
+class HauntedHouse implements Attraction {
+	private price = 500;
+
+	getPrice() {
+		return this.price;
+	}
+
+	accept(visitor: Visitor): void {
+		visitor.visitHauntedHouse(this);
+	}
+}
+
+class FerrisWheel implements Attraction {
+	private price = 30;
+
+	getPrice() {
+		return this.price;
+	}
+
+	accept(visitor: Visitor): void {
+		visitor.visitFerrisWheel(this);
+	}
+}
+
+class ChildVisitor implements Visitor {
+	visitRollerCoaster(rollerCoaster: RollerCoaster): void {
+		console.log(
+			`Niño en Montaña Rusa: Precio con descuento de $${
+				rollerCoaster.getPrice() * 0.5
+			}`
+		);
+	}
+	visitHauntedHouse(hauntedHouse: HauntedHouse): void {
+		console.log(
+			`Niño en Casa Embrujada: Precio con descuento de $${
+				hauntedHouse.getPrice() * 0.8
+			}`
+		);
+	}
+	visitFerrisWheel(ferrisWheel: FerrisWheel): void {
+		console.log(
+			`Niño en Rueda de la Fortuna: Precio con descuento de $${
+				ferrisWheel.getPrice() * 0.6
+			}`
+		);
+	}
+}
+
+class SeniorVisitor implements Visitor {
+	visitRollerCoaster(rollerCoaster: RollerCoaster): void {
+		console.log(
+			`Adulto en Montaña Rusa: Precio con descuento de $${
+				rollerCoaster.getPrice() * 0.5
+			}`
+		);
+	}
+	visitHauntedHouse(hauntedHouse: HauntedHouse): void {
+		console.log(
+			`Adulto en Casa Embrujada: Precio con descuento de $${
+				hauntedHouse.getPrice() * 0.5
+			}`
+		);
+	}
+	visitFerrisWheel(ferrisWheel: FerrisWheel): void {
+		console.log(
+			`Adulto en Rueda de la Fortuna: Precio con descuento de $${
+				ferrisWheel.getPrice() * 0.5
+			}`
+		);
+	}
+}
+
+class AdultVisitor implements Visitor {
+	visitRollerCoaster(rollerCoaster: RollerCoaster): void {
+		console.log(
+			`Adulto en Montaña Rusa: Precio con descuento de $${rollerCoaster.getPrice()}`
+		);
+	}
+	visitHauntedHouse(hauntedHouse: HauntedHouse): void {
+		console.log(
+			`Adulto en Casa Embrujada: Precio con descuento de $${hauntedHouse.getPrice()}`
+		);
+	}
+	visitFerrisWheel(ferrisWheel: FerrisWheel): void {
+		console.log(
+			`Adulto en Rueda de la Fortuna: Precio con descuento de $${ferrisWheel.getPrice()}`
+		);
+	}
+}
+
+function main() {
+	const attractions: Attraction[] = [
+		new RollerCoaster(),
+		new HauntedHouse(),
+		new FerrisWheel()
+	];
+
+	console.log("Visitante niño");
+	const childVisitor = new ChildVisitor();
+	attractions.forEach(attraction => attraction.accept(childVisitor));
+
+	console.log("");
+
+	console.log("Visitante adulto");
+	const adultVisitor = new AdultVisitor();
+	attractions.forEach(attraction => attraction.accept(adultVisitor));
+}
+
+main();
