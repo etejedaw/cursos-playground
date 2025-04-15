f09b1ecbeeb981676b243b3bf595f6e620d9c352
Esteban Tejeda <etejedaw@hotmail.com>
2025-04-14 20:33:15 -0400

    19-PDSPE: finalizado ejercicio patrón strategy

--- 19-PDSPE/03-comportamiento/08-strategy.ts
+++ 19-PDSPE/03-comportamiento/08-strategy.ts
@@ -17,3 +17,61 @@
  * ! patitos compiten en una carrera y cada uno tiene su propia
  * ! estrategia de movimiento (por ejemplo, nadar, volar o caminar).
  */
+
+interface MovementStrategy {
+	move(): void;
+}
+
+class SwimFast implements MovementStrategy {
+	move(): void {
+		console.log("El Pato nada rápidamente sobre el agua");
+	}
+}
+
+class FlyOverWater implements MovementStrategy {
+	move(): void {
+		console.log("El pato vuela elegantemente sobre el agua");
+	}
+}
+
+class WalkClumsily implements MovementStrategy {
+	move(): void {
+		console.log("El pato camina torpemente por la orilla");
+	}
+}
+
+class Duck {
+	constructor(
+		private name: string,
+		private movementStrategy: MovementStrategy
+	) {
+		console.log(`${this.name} listo para competir`);
+	}
+
+	performMove() {
+		console.log(`${this.name} se prepara para moverse`);
+		this.movementStrategy.move();
+	}
+
+	setMovementStrategy(strategy: MovementStrategy) {
+		this.movementStrategy = strategy;
+		console.log(`${this.name} cambió de estrategia`);
+	}
+}
+
+function main() {
+	const duck1 = new Duck("Patito rápido", new SwimFast());
+	const duck2 = new Duck("Patito volador", new FlyOverWater());
+	const duck3 = new Duck("Patito torpe", new WalkClumsily());
+
+	console.log("Comienza la patocarrera");
+
+	duck1.performMove();
+	duck2.performMove();
+	duck3.performMove();
+
+	duck3.setMovementStrategy(new FlyOverWater());
+	duck3.performMove();
+}
+
+main();
